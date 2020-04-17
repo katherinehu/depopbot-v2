df498b9d8d1983f9a35425d9963b86da84ce5e79
katherinehu <hu.z.katherine@gmail.com>
2020-04-17 15:30:08 -0400

    add info

--- bot.py
+++ bot.py
@@ -1,7 +1,15 @@
 import pyautogui, time
-recentItem = (837,629)
 
+recentItem = (100,600)
+buyNow = (361, 792)
+address = (140,590)
+
+order = [{'address': '6423 Tall Timbers Ct.', 'city':'Mason', 'zip':'45040'}]
 time.sleep(3)
-pyautogui.click(100,600)
+pyautogui.click(recentItem[0],recentItem[1])
 time.sleep(3)
 pyautogui.scroll(-150)
+pyautogui.click(buyNow[0],buyNow[1])
+pyautogui.click(address[0],address[1])
+
+pyautogui.typewrite()
