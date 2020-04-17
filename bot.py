4fb5e7574b8c95e93487855d30e8243e4c164305
katherinehu <hu.z.katherine@gmail.com>
2020-04-17 16:25:31 -0400

    imported findPrice

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import pyautogui, time
+import findPrice
 
 recentItem = (100,600)
 buyNow = (361, 792)
@@ -12,13 +13,22 @@ time.sleep(3)
 pyautogui.click(recentItem[0],recentItem[1])
 time.sleep(1)
 pyautogui.scroll(-150)
+
+# @clairehucodes: get url using urllib.request
+findPrice("https://www.depop.com/products/kaylakosuga-everything-posted-is-new-or-41/")
+
+# @clairehucodes: if price more than...
 pyautogui.click(buyNow[0],buyNow[1])
 pyautogui.click(address[0],address[1])
 
+
+
+
+'''
 pyautogui.typewrite(order['address'] + '\t')
 pyautogui.typewrite(order['city'] + '\t')
 #states
 pyautogui.typewrite(order['zip'] + '\t'+ '\t'+'\t')
 pyautogui.typewrite(order['phone'] + '\t')
-
+'''
 
--- findPrice.py
+++ findPrice.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
 
-def findPrice():
-    my_url = "https://www.depop.com/products/kaylakosuga-everything-posted-is-new-or-41/"
+def findPrice(my_url):
+
     #grab page
     uClient = uReq(my_url)
     page_html = uClient.read()
@@ -10,7 +10,7 @@ def findPrice():
     page_soup = soup(page_html, "html.parser")
 
     containers = page_soup.findAll("span",{"class": "Pricestyles__FullPrice-sc-1vj3zjr-0 fvDOul"})
+    price = containers[0].text.strip()
+    price = price[1:]
+    print(price)
 
-    print(containers)
-
-findPrice()
