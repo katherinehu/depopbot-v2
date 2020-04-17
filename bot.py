2be461721d6d41dda034a49fb2a8cbba74d7f858
katherinehu <hu.z.katherine@gmail.com>
2020-04-17 15:40:36 -0400

    update

--- bot.py
+++ bot.py
@@ -4,12 +4,21 @@ recentItem = (100,600)
 buyNow = (361, 792)
 address = (140,590)
 
-order = [{'address': '6423 Tall Timbers Ct.', 'city':'Mason', 'zip':'45040'}]
+order = [{'address': '6423 Tall Timbers Ct.',
+          'city':'Mason',
+          'zip':'45040'}]
 time.sleep(3)
 pyautogui.click(recentItem[0],recentItem[1])
-time.sleep(3)
+time.sleep(1)
 pyautogui.scroll(-150)
 pyautogui.click(buyNow[0],buyNow[1])
 pyautogui.click(address[0],address[1])
 
-pyautogui.typewrite()
+pyautogui.typewrite(order['address'] + '\t')
+pyautogui.typewrite(order['city'] + '\t')
+#states
+pyautogui.typewrite(order['zip'] + '\t'+ '\t')
+pyautogui.typewrite(order['address'] + '\t')
+pyautogui.typewrite(order['address'] + '\t')
+pyautogui.typewrite(order['address'] + '\t')
+
